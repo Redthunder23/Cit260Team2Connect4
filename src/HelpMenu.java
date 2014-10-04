1f9b89e1eec1f44d201b38fb2011d5dc9bcd43d6
osarahc <oes06002@byui.edu>
2014-10-04 13:03:20 -0600

    added two strings so far

---
+++ src/HelpMenu.java
@@ -0,0 +1,27 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+/**
+ *
+ * @author Owner
+ */
+public class HelpMenu {
+    //declare instance variables
+       type varI;
+       type varR;
+       type varO;
+       
+       //declare methods
+       type methodI(parameters){
+           //body of method
+       }
+       type methodR(parameters){
+           //body of method
+       }
+       type methodO(parameters){
+           //body of method
+       }
+    
+}
